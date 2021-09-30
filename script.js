c0db5bd830c7182dec43b3970daf237f3f8293b8
Derek Banister <ptownderek@yahoo.com>
2021-09-29 22:13:30 -0700

    localStorage fixed. Was only targeting one textarea and now targeting all

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 //First i need to display time and date in the jumbotron on page load
 //Using moment and the id of currentDay
-
-
 $(document).ready(function () {
     
     var now = new Date().getHours();
@@ -18,18 +16,19 @@ $(document).ready(function () {
 
     //Give button onclick that runs a function that saves the input text and
     //adds to local storage.
-    //still an issue with local storage here
-    $(".hourBlock").each(function (i, e) {
+    //local storage fixed. Reached out to bcs learning assistant and got great help
+    //Issue was on line 28 regarding me targeting only one text area for the value
+    //I am storing
+    $(".hourBlock").each(function (i) {
         //console.log(i)
-        //console.log(e)
         //console.log(this); 
         const parent = $(this).children()[2];
         $(parent).children().on("click", function (event) {
 
             event.preventDefault();
-            var text = $(".textSave").val();
+            var text = $(this).parent().siblings(".textSave").val()
 
-            localStorage.setItem(("text-" + i++), text);
+            localStorage.setItem(("text-" + i), text);
 
         })
     })
